97b9c0686a4e9527044fa2ebb8d0c7da2fadc70d
Ilmari Vacklin <ilmari.vacklin@reaktor.fi>
2014-10-08 15:25:24 +0300

    chapter 6

--- plai-typed-racket.rkt
+++ plai-typed-racket.rkt
@@ -1,22 +1,13 @@
 #lang typed/racket
 
+(require typed/rackunit)
+
 (define-syntax deftype
   (syntax-rules ()
     [(_ name [cons-name (field-name : field-type) ...] ...)
      (begin
        (struct cons-name ([field-name : field-type] ...) #:transparent) ...
        (define-type name (U cons-name ...)))]))
-
-;; (deftype arith-s
-;;   [num-s (n : Number)]
-;;   [plus-s (l : arith-s) (r : arith-s)]
-;;   [bminus-s (l : arith-s) (r : arith-s)]
-;;   [mult-s (l : arith-s) (r : arith-s)])
-
-;; (deftype arith-c
-;;   [num-c (n : Number)]
-;;   [plus-c (l : arith-c) (r : arith-c)]
-;;   [mult-c (l : arith-c) (r : arith-c)])
 
 (deftype expr-c
   [num-c (n : Number)]
@@ -31,59 +22,55 @@
 (deftype Binding
   [bind (name : Symbol) (val : Number)])
 
-(define-type Env Binding)
+(define-type Env (Listof Binding))
 (define mt-env empty)
 (define extend-env cons)
 
-(: interp (expr-c (Listof fun-def-c) -> Number))
-(define (interp e fds)
-  (match e
+(: interp (expr-c Env (Listof fun-def-c) -> Number))
+(define (interp expr env fds)
+  (match expr
     [(num-c n) n]
-    [(plus-c l r) (+ (interp l fds) (interp r fds))]
-    [(mult-c l r) (* (interp l fds) (interp r fds))]
+    [(plus-c l r) (+ (interp l env fds) (interp r env fds))]
+    [(mult-c l r) (* (interp l env fds) (interp r env fds))]
     [(app-c f a) (let ([fd : fun-def-c (get-fundef f fds)])
-                       (interp (subst (interp a fds)
-                                      (fd-c-arg fd)
-                                      (fd-c-body fd))
+                       (interp (fd-c-body fd)
+                               (extend-env (bind (fd-c-arg fd)
+                                                 (interp a env fds))
+                                           mt-env)
                                fds))]
-    [(id-c s) (error 'interp "shouldn't get here")]))
+    [(id-c n) (lookup n env)]))
+
+(: lookup (Symbol Env -> Number))
+(define (lookup name env)
+   (let ([binding (findf (match-lambda [(bind n _) (symbol=? n name)]) env)])
+     (if binding
+         (bind-val binding)
+         (error 'lookup "no such binding"))))
 
 (: get-fundef (Symbol (Listof fun-def-c) -> fun-def-c))
 (define (get-fundef name fds)
   (or (findf (match-lambda [(fd-c n _ _) (symbol=? n name)]) fds)
       (error "oops")))
 
-(: subst (Number Symbol expr-c -> expr-c))
-(define (subst what for in)
-  (define with (num-c what))
-  (: go (expr-c -> expr-c))
-  (define/match (go in)
-    [((num-c n)) in]
-    [((id-c s)) (if (symbol=? s for) with in)]
-    [((app-c f a)) (app-c f (go a))]
-    [((plus-c l r)) (plus-c (go l) (go r))])
-  (go in))
+(check-equal? (interp (plus-c (num-c 10) (app-c 'const-5 (num-c 10)))
+                      mt-env
+                      (list (fd-c 'const-5 '_ (num-c 5))))
+              15)
 
-;; (: desugar (arith-s -> arith-c))
-;; (define (desugar expr)
-;;   (match expr
-;;     [(num-s n) (num-c n)]
-;;     [(plus-s l r) (plus-c (desugar l) (desugar r))]
-;;     [(mult-s l r) (mult-c (desugar l) (desugar r))]
-;;     [(bminus-s l r) (plus-c (desugar l) (mult-c (num-c -1) (desugar r)))]))
+(check-equal? (interp (plus-c (num-c 10) (app-c 'double (plus-c (num-c 1) (num-c 2))))
+                      mt-env
+                      (list (fd-c 'double 'x (plus-c (id-c 'x) (id-c 'x)))))
+              16)
 
-;; (: eval-arith (arith-c -> Number))
-;; (define (eval-arith expr)
-;;   (match expr
-;;     [(num-c n) n]
-;;     [(plus-c l r) (+ (eval-arith l) (eval-arith r))]
-;;     [(mult-c l r) (* (eval-arith l) (eval-arith r))]))
+(check-equal? (interp (plus-c (num-c 10) (app-c 'quadruple (plus-c (num-c 1) (num-c 2))))
+                      mt-env
+                      (list (fd-c 'quadruple 'x (app-c 'double (app-c 'double (id-c 'x))))
+                            (fd-c 'double 'x (plus-c (id-c 'x) (id-c 'x)))))
+              22)
 
-;; (: parse-arith (Sexp -> arith-c))
-;; (define (parse-arith expr)
-;;   (match expr
-;;     [(? number? n) (num-c n)]
-;;     [(list '+ l r) (plus-c (parse-arith l) (parse-arith r))]
-;;     [(list '* l r) (mult-c (parse-arith l) (parse-arith r))]))
-
-;; (eval-arith (parse-arith '(+ (* 42 2) (* 2 (+ 1 0)))))
+(check-exn
+ exn:fail?
+ (lambda () (interp (app-c 'f1 (num-c 3))
+               mt-env
+               (list (fd-c 'f1 'x (app-c 'f2 (num-c 4)))
+                     (fd-c 'f2 'y (plus-c (id-c 'x) (id-c 'y)))))))
